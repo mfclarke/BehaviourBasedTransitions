0f3a4090f96ada44b099dd8870ba2f6fb57f41aa
Maximilian Clarke <maximilianfc@gmail.com>
2016-10-28 07:05:30 +0200

    Adding quick/nimble frameworks to unit test target, creating first test

---
+++ BehaviourBasedTransitionsTests/BehaviourBasedTransitionSpec.swift
@@ -0,0 +1,28 @@
+//
+//  BehaviourBasedTransitionSpec.swift
+//  BehaviourBasedTransitions
+//
+//  Created by Maximilian Clarke on 28/10/2016.
+//  Copyright © 2016 Maximilian Clarke. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import BehaviourBasedTransitions
+
+class BehaviourBasedTransitionSpec: QuickSpec {
+    
+    override func spec() {
+
+        describe("Something") {
+            
+            it("should fail") {
+                fail()
+            }
+            
+        }
+        
+    }
+    
+}
